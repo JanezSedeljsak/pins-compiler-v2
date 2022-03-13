5ef1e3314ed564831e56bd2198ca1c70b6dde692
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-03-13 23:48:40 +0100

    lexical analysis & tests

--- pins/data/symbol/Token.java
+++ pins/data/symbol/Token.java
@@ -1,7 +1,112 @@
 package pins.data.symbol;
 
-public enum Token {
+import pins.common.report.*;
 
-	EOF
+public enum Token {
 	
+	EOF,
+
+	/* constants */
+	VOID_CONST("none"), INT_CONST("[0-9]+", true), CHAR_CONST, POINTER_CONST("nil"),
+
+	/* keywoards */
+	CHAR("char"), DEL("del"), DO("do"), ELSE("else"), END("end"), FUN("fun"), IF("if"), INT("int"), 
+	NEW("new"), THEN("then"), TYP("typ"), VAR("var"), VOID("void"), WHERE("where"), WHILE("while"),
+
+	/* brackets */
+	LEFT_BRACKET("["), RIGHT_BRACKET("]"),
+	LEFT_BRACE("{"), RIGHT_BRACE("}"),
+	LEFT_PARENTHESIS("("), RIGHT_PARENTHESIS(")"),
+
+	/* symbols */
+	COMMA(","), COLON(":"), SEMICOLON(";"), AND("&"), OR("|"), NEGATION("!"), EQUAL("=="), NOT_EQUAL("!="), 
+	LESS("<"), GREATER(">"), LESS_OR_EQUAL("<="), GREATER_OR_EQUAL(">="),
+	
+	/* math */
+	MULTIPLY("*"), DIVIDE("/"), MOD("%"), PLUS("+"),
+	MINUS("-"), POW("^"), ASSIGN("="),
+
+	IDENTIFIER("[A-Za-z_][A-Za-z0-9_]*", true);
+
+	private String match;
+	private boolean isRegex;
+	private static char FIRST_PRINTABLE = ' ', LAST_PRINTABLE = '~'; // 32, 126
+
+	Token() {
+		this.match = null;
+		this.isRegex = false;
+	}
+
+	Token(String match) {
+		this();
+		this.match = match;
+	}
+
+	Token(String match, boolean isRegex) {
+		this.match = match;
+		this.isRegex = isRegex;
+	}
+
+	// a bit of a hacky solution for character matching (implemented like this for easier error tracking)
+	private static boolean handleCharacterMatching(String match, boolean fullMatch, int row, int col) {
+		int matchLength = match.length();
+
+		try {
+			if (matchLength == 0 || match.charAt(0) != '\'') return false;
+			// from here forward it must be a char contstant (if it won't match error will be thrown)
+
+			if (matchLength == 1 && !fullMatch) return true;
+
+			if (!(FIRST_PRINTABLE <= match.charAt(1) && match.charAt(1) <= LAST_PRINTABLE && match.charAt(1) != '\'')) {
+				throw new Report.Error(new Location(row, col + 1), String.format("Invalid character symbol %s", String.valueOf(match.charAt(1))));
+			}
+
+			if (matchLength == 2 && !fullMatch) return true;
+
+			if (match.charAt(1) == '\\') {
+				if (match.charAt(2) != '\'' && match.charAt(2) != '\\') {
+					throw new Report.Error(new Location(row, col + 2), String.format("Invalid character symbol after escape character \\%s", String.valueOf(match.charAt(2))));
+				}
+
+				if (matchLength == 3  && !fullMatch) return true;
+				if (matchLength > 3 && match.charAt(3) == '\'') return matchLength == 4;
+
+				throw new Report.Error(new Location(row, col + 3), "Missing closing tag for character");
+
+			} else if (match.charAt(2) == '\'') {
+				return matchLength == 3;
+			}
+				
+			int charIndex = match.charAt(1) == '\\' ? 2 : 1;
+			throw new Report.Error(new Location(row, col + charIndex), String.format("Invalid closing tag for character: %s", String.valueOf(match.charAt(charIndex))));
+			
+
+		} catch (Report.Error __) {
+			System.exit(1);
+		}
+
+		return false;
+	}
+
+	public static Token getFirstMatch(String match, boolean fullMatch, int row, int col) {
+		if (match == null) return null;
+		int matchLength = match.length();
+
+		boolean isCharMatch = handleCharacterMatching(match, fullMatch, row, col);
+		if (isCharMatch) return Token.CHAR_CONST;
+
+		for (Token token: Token.values()) {
+			if (token.match == null) continue;
+			
+			if (!token.isRegex) {
+				if (fullMatch && token.match.equals(match)) return token;
+				if (!fullMatch && token.match.substring(0, Math.min(token.match.length(), matchLength)).equals(match)) {
+					return token;
+				}
+
+			} else if (match.matches(token.match)) return token;
+		}
+
+		return null;
+	}
 }
--- pins/phase/lexan/LexAn.java
+++ pins/phase/lexan/LexAn.java
@@ -1,6 +1,8 @@
 package pins.phase.lexan;
 
 import java.io.*;
+import java.util.*;
+
 import pins.common.report.*;
 import pins.data.symbol.*;
 
@@ -10,9 +12,14 @@ import pins.data.symbol.*;
 public class LexAn implements AutoCloseable {
 
 	private String srcFileName;
-
 	private FileReader srcFile;
+	private final static int TAB_SIZE = 8;
 
+	private int row = 1;
+	private int col = 0;
+
+	private Queue<Character> unseen = new LinkedList<>();
+	
 	public LexAn(String srcFileName) {
 		this.srcFileName = srcFileName;
 		try {
@@ -30,7 +37,137 @@ public class LexAn implements AutoCloseable {
 		}
 	}
 
-	public Symbol lexer() {
+	private char getNextCharacter() {
+		try {
+			if (unseen.isEmpty()) {
+				col++;
+				return (char) srcFile.read(); 
+			}
+
+			return unseen.remove();
+
+		} catch (IOException __) {
+			throw new Report.Error("Error reading from source file '" + srcFileName + "'.");
+		}
 	}
 
+	private boolean isEOF(char chr) {
+		return (char)chr == (char)-1;
+	}
+
+	private boolean isNotWhiteSpace(char chr) {
+		return !String.valueOf(chr).matches("\\s+");
+	}
+
+	private Symbol passComment() {
+		int commentDepth = 1;
+		char currChar = getNextCharacter();
+		if (currChar != '{') {
+			throw new Error(String.format("Invalid start of comment #%c", currChar));
+		}
+
+		while ((currChar = getNextCharacter()) != (char)-1) {
+			if (currChar == '#' && getNextCharacter() == '{') commentDepth++;
+			else if (currChar == '}') {
+				char next;
+
+				// handle eg. '#{  this is a comment }}}}}}#'
+				while ((next = getNextCharacter()) == '}') {}
+				if (next == '#') {
+					commentDepth--;
+				}
+
+			} else if (currChar == '\n') {
+				row++;
+				col = 0;
+			}
+
+			if (commentDepth == 0) break;
+		}
+
+		if (currChar == (char)-1) {
+			if (commentDepth != 0) Report.warning("Comment not closed at end of file!");
+			return new Symbol(Token.EOF, "", null);
+		}
+
+		return null;
+	}
+
+	public Symbol lexer() {
+		StringBuilder builder = new StringBuilder("");
+		boolean eofFlag = false, notWhiteSpaceFlag = false, noMathFlag = false;
+		int len;
+		char current;
+		
+		// inner loop breaks out everytime we see a whitespace, however, that does not mean we found a valid symbol
+		// this is why we break out to the outer loop and run until we find a valid next symbol 
+		while (true) {
+			while (true) {
+
+				// read next character and set flags
+				current = getNextCharacter();
+				eofFlag = isEOF(current);
+				notWhiteSpaceFlag = isNotWhiteSpace(current);
+
+				if (builder.length() > 0 || notWhiteSpaceFlag) {
+					builder.append(current);
+				}
+
+				// handle eof and newline
+				if (eofFlag) break;
+				if (current == '\n') {
+					row++;
+					col = 0;
+				}
+
+				// handle tab
+				if (current == '\t') {
+					col = ((col / TAB_SIZE) + 1) * TAB_SIZE;
+					break;
+				}
+
+				// handle single whitespace (if char was seen don't break)
+				boolean prevIsCharStart = builder.length() > 0 && builder.toString().charAt(builder.length() - 1) == '\'';
+				if (current == ' ' && builder.length() > 0 && prevIsCharStart) break;
+
+				// handle comment start
+				if (current == '#') {
+					Symbol eofSymbol = passComment();
+					builder.setLength(0); // clear buffer after comment
+					if (eofSymbol != null) return eofSymbol;
+				}
+	
+				// break and return last valid lexeme without last token
+				noMathFlag = Token.getFirstMatch(builder.toString(), false, row, col) == null;
+				if (noMathFlag) break;
+			}
+
+			// store token if not comment, whitespace (allow other characters or EOF)
+			if (current != '#' && (notWhiteSpaceFlag || eofFlag)) {
+				unseen.add(current);
+			}
+
+			// handle EOF
+			len = builder.length();
+			if (len == 0 || (len == 1 && eofFlag)) {
+				if (eofFlag) return new Symbol(Token.EOF, "", null);
+				continue;
+			}
+
+			// print invalid token error
+			if (noMathFlag && len == 1) {
+				throw new Report.Error(new Location(row, col), String.format("Invalid token: %s", builder.toString()));
+			}
+
+			// parse and return valid symbol
+			builder.setLength(builder.length() - 1);
+			String lexeme = builder.toString();
+			Token matchedToken = Token.getFirstMatch(lexeme, true, row, col);
+
+			if (matchedToken != null) {
+				return new Symbol(matchedToken, lexeme, new Location(row, col - lexeme.length(), row, col - 1));
+			}
+		}
+	}
 }
+ 
