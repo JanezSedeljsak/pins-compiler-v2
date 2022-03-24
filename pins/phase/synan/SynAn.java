af1d16859bea25077b1a60a76ab94f657c037236
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-03-24 13:47:16 +0100

    basic structure for syntax analysis

---
+++ pins/phase/synan/SynAn.java
@@ -0,0 +1,16 @@
+package pins.phase.synan;
+
+import pins.phase.lexan.*;
+
+public class SynAn implements AutoCloseable {
+
+	public SynAn(LexAn lexan) {		
+	}
+	
+	public void close() {
+	}
+	
+	public void parser() {
+	}
+
+}
