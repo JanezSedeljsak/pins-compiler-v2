f1b264a7a502e50f8e6bcccbd536816e9c97bd98
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-03-27 15:09:33 +0200

    expression parsing

--- pins/phase/synan/SynAn.java
+++ pins/phase/synan/SynAn.java
@@ -7,7 +7,7 @@ import pins.common.report.*;
 public class SynAn implements AutoCloseable {
 
 	private final LexAn lexan;
-	private Symbol currSymb = null, prevSymb = null;
+	private Symbol current = null, prevSymb = null;
 	private SynNode syntree;
 	private boolean dontMove = false; 
 
@@ -27,7 +27,7 @@ public class SynAn implements AutoCloseable {
 	}
 
 	public Symbol peek() {
-		return currSymb;
+		return current;
 	}
 
 	public void move() {
@@ -35,7 +35,7 @@ public class SynAn implements AutoCloseable {
 			dontMove = false;
 		} else {
 			prevSymb = peek();
-			currSymb = lexan.lexer();
+			current = lexan.lexer();
 		}
 	}
 
@@ -55,10 +55,10 @@ public class SynAn implements AutoCloseable {
 
 	private void parseDecls() {
 		this.syntree = new SynNode("ROOT");
-		parseDecl();
+		parseDecl(this.syntree);
 	}
 
-	private void parseDecl() {
+	private void parseDecl(SynNode parentNode) {
 		SynNode declNode = new SynNode("DECL");
 		move();
 		switch (peek().token) {
@@ -87,13 +87,14 @@ public class SynAn implements AutoCloseable {
 				addExpected(declNode, Token.COLON);
 				parseType(declNode);
 				addExpected(declNode, Token.ASSIGN);
+				parseExpr(declNode);
 				break;
 			default:
 				throw new Report.Error(peek().location, String.format("Unexpected token at start of DECL: %s", peek().lexeme));
 		}
 
 		Report.info(declNode.toString());
-		syntree.addNode(declNode);
+		parentNode.addNode(declNode);
 		parseDecls();
 	}
 
@@ -108,6 +109,10 @@ public class SynAn implements AutoCloseable {
 				typeNode.addNodeSymbol(peek());
 				break;
 			case LEFT_BRACKET:
+				typeNode.addNodeSymbol(peek());
+				parseExpr(typeNode);
+				addExpected(typeNode, Token.RIGHT_BRACKET);
+				parseType(typeNode);
 				break;
 			case POINTER:
 				typeNode.addNodeSymbol(peek());
@@ -145,35 +150,310 @@ public class SynAn implements AutoCloseable {
 	}
 
 	private void parseOptionalParams(SynNode parentNode) {
-		SynNode paramNode = new SynNode("OPTIONAL_PARAMS");
+		SynNode optionalParamsNode = new SynNode("OPTIONAL_PARAMS");
 		move();
 		switch (peek().token) {
 			case COMMA:
-				paramNode.addNodeSymbol(peek());
-				addExpected(paramNode, Token.IDENTIFIER);
-				addExpected(paramNode, Token.COLON);
-				parseType(paramNode);
-				parseOptionalParams(paramNode);
+				optionalParamsNode.addNodeSymbol(peek());
+				addExpected(optionalParamsNode, Token.IDENTIFIER);
+				addExpected(optionalParamsNode, Token.COLON);
+				parseType(optionalParamsNode);
+				parseOptionalParams(optionalParamsNode);
 				break;
 			default:
 				dontMove = true;
 		}
 
-		parentNode.addNode(paramNode);
-		Report.info(paramNode.toString());
+		parentNode.addNode(optionalParamsNode);
+		Report.info(optionalParamsNode.toString());
 	}
 
 	private void parseExpr(SynNode parentNode) {
 		SynNode exprNode = new SynNode("EXPR");
-		move();
-		switch (peek().token) {
-			case INT:
-				break;
-			default:
-				throw new Report.Error(peek().location, String.format("Unexpected token at start of TYPE: %s", peek().lexeme));
-		}
-
+		parseAnd(exprNode);
+		parseOr(exprNode);
 		parentNode.addNode(exprNode);
 		Report.info(exprNode.toString());
+	}
+
+	private void parseOr(SynNode parentNode) {
+		SynNode orNode = new SynNode("OR");
+		switch (peek().token) {
+			case OR:
+				orNode.addNodeSymbol(peek());
+				parseAnd(orNode);
+				parseOr(orNode);
+				break;
+			default:
+				dontMove = true;
+		}
+
+		parentNode.addNode(orNode);
+		Report.info(orNode.toString());
+	}
+
+	private void parseAnd(SynNode parentNode) {
+		SynNode andNode = new SynNode("AND");
+		parseRelational(andNode);
+		parseInnerAnd(andNode);
+		parentNode.addNode(andNode);
+		Report.info(andNode.toString());
+	}
+
+	private void parseInnerAnd(SynNode parentNode) {
+		SynNode innerAndNode = new SynNode("AND_INNER");
+		switch (peek().token) {
+			case AND:
+				innerAndNode.addNodeSymbol(peek());
+				parseRelational(innerAndNode);
+				parseInnerAnd(innerAndNode);
+				break;
+			default:
+				dontMove = true;
+		}
+
+		parentNode.addNode(innerAndNode);
+		Report.info(innerAndNode.toString());
+	}
+
+	private void parseRelational(SynNode parentNode) {
+		SynNode relationalNode = new SynNode("RELATIONAL");
+		parseAddSub(relationalNode);
+		parseInnerRelational(relationalNode);
+		parentNode.addNode(relationalNode);
+		Report.info(relationalNode.toString());
+	}
+
+	private void parseInnerRelational(SynNode parentNode) {
+		SynNode innerRelationalNode = new SynNode("RELATIONAL_INNER");
+		switch (peek().token) {
+			case EQUAL:
+			case NOT_EQUAL:
+			case LESS:
+			case GREATER:
+			case LESS_OR_EQUAL:
+			case GREATER_OR_EQUAL:
+				innerRelationalNode.addNodeSymbol(peek());
+				parseAddSub(innerRelationalNode);
+				parseInnerRelational(innerRelationalNode);
+				break;
+			default:
+				dontMove = true;
+		}
+
+		parentNode.addNode(innerRelationalNode);
+		Report.info(innerRelationalNode.toString());
+	}
+
+	private void parseAddSub(SynNode parentNode) {
+		SynNode addSubNode = new SynNode("ADD_SUB");
+		parseOtherMath(addSubNode);
+		parseInnerAddSub(addSubNode);
+		parentNode.addNode(addSubNode);
+		Report.info(addSubNode.toString());
+	}
+
+	private void parseInnerAddSub(SynNode parentNode) {
+		SynNode innerAddSubNode = new SynNode("INNER_ADD_SUB");
+		switch (peek().token) {
+			case PLUS:
+			case MINUS:
+				innerAddSubNode.addNodeSymbol(peek());
+				parseOtherMath(innerAddSubNode);
+				parseInnerAddSub(innerAddSubNode);
+				break;
+			default:
+				dontMove = true;
+		}
+
+		parentNode.addNode(innerAddSubNode);
+		Report.info(innerAddSubNode.toString());
+	}
+
+	private void parseOtherMath(SynNode parentNode) {
+		SynNode relationalNode = new SynNode("OTHER_MATH");
+		parsePrefix(relationalNode);
+		parseInnerOtherMath(relationalNode);
+		parentNode.addNode(relationalNode);
+		Report.info(relationalNode.toString());
+	}
+
+	private void parseInnerOtherMath(SynNode parentNode) {
+		SynNode innerOtherMathNode = new SynNode("INNER_OTHER_MATH");
+		switch (peek().token) {
+			case MULTIPLY:
+			case DIVIDE:
+			case MOD:
+				innerOtherMathNode.addNodeSymbol(peek());
+				parsePrefix(innerOtherMathNode);
+				parseInnerOtherMath(innerOtherMathNode);
+				break;
+			default:
+				dontMove = true;
+		}
+
+		parentNode.addNode(innerOtherMathNode);
+		Report.info(innerOtherMathNode.toString());
+	}
+
+	private void parsePrefix(SynNode parentNode) {
+		SynNode prefixNode = new SynNode("PREFIX");
+		switch (peek().token) {
+			case NEGATION:
+			case PLUS:
+			case MINUS:
+			case POINTER:
+				prefixNode.addNodeSymbol(peek());
+				parsePrefix(prefixNode);
+				break;
+			default:
+				parsePostfix(prefixNode);
+		}
+
+		parentNode.addNode(prefixNode);
+		Report.info(prefixNode.toString());
+	}
+
+	private void parsePostfix(SynNode parentNode) {
+		SynNode postfixNode = new SynNode("POSTFIX");
+		parseExprWrapper(postfixNode);
+		parseInnerPostfix(postfixNode);
+		Report.info(postfixNode.toString());
+	}
+
+	private void parseInnerPostfix(SynNode parentNode) {
+		SynNode postfixNode = new SynNode("INNER_POSTFIX");
+		switch (peek().token) {
+			case LEFT_BRACKET:
+				postfixNode.addNodeSymbol(peek());
+				parseExpr(postfixNode);
+				addExpected(postfixNode, Token.RIGHT_BRACKET);
+				break;
+			case POINTER:
+				postfixNode.addNodeSymbol(peek());
+				parseInnerPostfix(postfixNode);
+				break;
+			default:
+				dontMove = true;
+		}
+		
+		parentNode.addNode(postfixNode);
+		Report.info(postfixNode.toString());
+	}
+
+	private void parseExprWrapper(SynNode parentNode) {
+		SynNode exprWrapperNode = new SynNode("EXPR_WRAPPER");
+		switch (peek().token) {
+			case INT_CONST:
+			case CHAR_CONST:
+			case POINTER_CONST:
+			case VOID_CONST:
+				exprWrapperNode.addNodeSymbol(peek());
+				break;
+			case IDENTIFIER:
+				exprWrapperNode.addNodeSymbol(peek());
+				parseFuncCall(exprWrapperNode);
+				break;
+			case NEW:
+			case DEL:
+				exprWrapperNode.addNodeSymbol(peek());
+				parseExprWrapper(exprWrapperNode);
+				break;
+			case LEFT_BRACE:
+				exprWrapperNode.addNodeSymbol(peek());
+				parseStmt(exprWrapperNode);
+				parseStmts(exprWrapperNode);
+				addExpected(exprWrapperNode, Token.RIGHT_BRACE);
+				break;
+			case LEFT_PARENTHESIS:
+				exprWrapperNode.addNodeSymbol(peek());
+				parseExpr(exprWrapperNode);
+				parseExprEnd(exprWrapperNode);
+				addExpected(exprWrapperNode, Token.RIGHT_PARENTHESIS);
+				break;
+			default:
+				throw new Report.Error(peek().location, String.format("Unexpected token at start of EXPR: %s", peek().lexeme));
+		}
+		
+		parentNode.addNode(exprWrapperNode);
+		Report.info(exprWrapperNode.toString());
+	}
+
+	private void parseExprEnd(SynNode parentNode) {
+		SynNode exprEndNode = new SynNode("EXPR_END");
+		switch (peek().token) {
+			case COLON:
+				exprEndNode.addNodeSymbol(peek());
+				parseType(exprEndNode);
+				break;
+			case WHERE:
+				exprEndNode.addNodeSymbol(peek());
+				parseDecl(exprEndNode);
+				break;
+			default:
+				dontMove = true;
+		}
+		parentNode.addNode(exprEndNode);
+		Report.info(exprEndNode.toString());
+	}
+
+	private void parseFuncCall(SynNode parentNode) {
+		SynNode funcCallNode = new SynNode("FUNC_PARAMS");
+		switch (peek().token) {
+			case LEFT_PARENTHESIS:
+				funcCallNode.addNodeSymbol(peek());
+				parseCallParams(funcCallNode);
+				addExpected(funcCallNode, Token.RIGHT_PARENTHESIS);
+				break;
+			default:
+				dontMove = true;
+		}
+		
+		parentNode.addNode(funcCallNode);
+		Report.info(funcCallNode.toString());
+	}
+
+	private void parseCallParams(SynNode parentNode) {
+		SynNode callParamsNode = new SynNode("FUNC_PARAMS");
+		switch (peek().token) {
+			case IDENTIFIER:
+			case LEFT_PARENTHESIS:
+			case LEFT_BRACE:
+			case POINTER:
+			case PLUS:
+			case MINUS:
+			case NEGATION:
+			case VOID_CONST:
+			case INT_CONST:
+			case CHAR_CONST:
+			case POINTER_CONST:
+			case NEW:
+			case DEL:
+				callParamsNode.addNodeSymbol(peek());
+				parseExpr(callParamsNode);
+				parseOptionalCallParams(callParamsNode);
+				break;
+			default:
+				dontMove = true;
+		}
+		
+		parentNode.addNode(callParamsNode);
+		Report.info(callParamsNode.toString());
+	}
+
+	private void parseOptionalCallParams(SynNode parentNode) {
+		SynNode optionalCallParamsNode = new SynNode("FUNC_PARAMS");
+		switch (peek().token) {
+			case COMMA:
+				optionalCallParamsNode.addNodeSymbol(peek());
+				parseExpr(optionalCallParamsNode);
+				parseOptionalCallParams(parentNode);
+				break;
+			default:
+				dontMove = true;
+		}
+		
+		parentNode.addNode(optionalCallParamsNode);
+		Report.info(optionalCallParamsNode.toString());
 	}
 }
