39c3ef1c5beece58ff67726195f583a3f559eb09
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-05-18 19:55:32 +0200

    move result to RV

--- pins/phase/imcgen/CodeGenerator.java
+++ pins/phase/imcgen/CodeGenerator.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 import pins.data.ast.*;
 import pins.data.ast.visitor.*;
+import pins.data.imc.code.expr.*;
+import pins.data.imc.code.stmt.ImcMOVE;
 import pins.data.mem.*;
 import pins.phase.memory.*;
 
@@ -17,7 +19,10 @@ public class CodeGenerator extends AstFullVisitor<Object, Stack<MemFrame>> {
 
 		MemFrame frame = Memory.frames.get(funDecl);
 		arg.add(frame);
-		funDecl.expr.accept(new ExprGenerator(), arg);
+		ImcExpr expr = funDecl.expr.accept(new ExprGenerator(), arg);
+		ImcTEMP rvTemp = new ImcTEMP(arg.peek().RV);
+		ImcMOVE writeRV = new ImcMOVE(rvTemp, expr);
+		//ImcGen.exprImc.put(funDecl.expr, new ImcSEXPR(writeRV, rvTemp));
 		arg.pop();
 		
 		return null;
